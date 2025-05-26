f9db2345a4574c82de352e173969fb5fd764bba3
jumin09 <gjumin4070@gmail.com>
2025-05-26 22:02:49 +0900

    UI 다 만듦

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,47 @@
-
+import { VscChevronDown } from "react-icons/vsc";
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <header>
-       <div className='head'>
-          <div>
-            <img src='/public/img/logo1.png' alt="logo" className='logo'/>
+      <header className="header">"
+        <div className="head">
+          <div className="head-left">
+            <img src="/image1.png" alt="logo" className="logo" />
           </div>
-          <div>
-            <span className='title'>학습계획</span>
+          <div className="head-center">
+            <span className="title">학습계획</span>
           </div>
-          <div className='account'>
-              <img alt='계정사진' className='account-img'/>
-              <span id='account_name'>계정이름</span>
+          <div className="head-right">
+            <img alt="계정사진" className="account-img" />
+            <div id="account_name">계정이름</div>
           </div>
         </div>
-        <div className='naljja'>
-            <span id=''></span> <span></span>
+        <div className='naljja_body'>
+            <span className='naljja'>2025.05</span><div className="Icon"><VscChevronDown size="30"/></div>
+        </div>
+        <div className="em"></div>
+        <div className="week">
+          <div className="weekdays">
+            <div className="Sun">Sun</div>
+            <div>Mon</div>
+            <div>Tue</div>
+            <div>Wed</div>
+            <div>Thu</div>
+            <div>Fri</div>
+            <div>Sat</div>
+          </div>
         </div>
       </header>
+      <main>
+        <div className="calendar">
+          {[...Array(35)].map((_, i) => (
+            <div className="cell" key={i}></div>
+          ))}
+        </div>
+      </main>
+
     </>
   )
 }
