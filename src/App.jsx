07107586ca6601c88e2a91973fb2f02f11e80fa7
fany0322 <fanystar0322@gmail.com>
2025-06-11 16:55:42 +0900

    라우팉ㅇ까지했음

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,41 @@
-import { VscChevronDown } from "react-icons/vsc";
+import { useNavigate } from "react-router-dom"
+import { VscChevronDown } from "react-icons/vsc"
 import './App.css'
 
-function App() {
+function App({ loggedIn }) {
+  const navigate = useNavigate()
 
   return (
     <>
-      <header className="header">"
+      <header className="header">
         <div className="head">
           <div className="head-left">
             <img src="/image1.png" alt="logo" className="logo" />
           </div>
+
           <div className="head-center">
             <span className="title">학습계획</span>
           </div>
+
           <div className="head-right">
-            <img alt="계정사진" className="account-img" />
-            <div id="account_name">계정이름</div>
+            {!loggedIn ? (
+              <button onClick={() => navigate('/logins')}>
+                로그인
+              </button>
+            ) : (
+              <>
+                <img src="/default_profile.png" alt="계정사진" className="account-img" />
+                <div id="account_name">계정이름</div>
+              </>
+            )}
           </div>
         </div>
-        <div className='naljja_body'>
-            <span className='naljja'>2025.05</span><div className="Icon"><VscChevronDown size="30"/></div>
+
+        <div className="naljja_body">
+          <span className="naljja">2025.05</span>
+          <div className="Icon"><VscChevronDown size="30" /></div>
         </div>
-        <div className="em"></div>
+
         <div className="week">
           <div className="weekdays">
             <div className="Sun">Sun</div>
@@ -34,22 +48,32 @@ function App() {
           </div>
         </div>
       </header>
-      <main>
-        <div className="calendar">
-          {[...Array(35)].map((_, i) => (
-            <div className="cell" key={i}></div>
-          ))}
-        </div>
-      </main>
-      <div className='bottom'>
-      <div className="toolbar1">
-        <button className="toolbutton">+</button>
-      </div>
-      <div className="toolbar2">
-        <button className="toolbutton">+</button>
-      </div>
-      </div>
 
+      <main>
+        {!loggedIn ? (
+          <div className="auth-container">
+            <div className="login-pil">로그인이 필요합니다</div>
+            <button onClick={() => navigate('/logins')}>로그인하러 가기</button>
+          </div>
+        ) : (
+          <div className="calendar">
+            {[...Array(35)].map((_, i) => (
+              <div className="cell" key={i}></div>
+            ))}
+          </div>
+        )}
+      </main>
+
+      {loggedIn && (
+        <div className='bottom'>
+          <div className="toolbar1">
+            <button className="toolbutton">+</button>
+          </div>
+          <div className="toolbar2">
+            <button className="toolbutton">+</button>
+          </div>
+        </div>
+      )}
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,16 @@
 import { createRoot } from 'react-dom/client'
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import Login from './login.jsx'
 import Signup from './singup/Singup.jsx'
 import Logins from './logins/logins.jsx'
 
-function Root() {
-  const [screen, setScreen] = useState('login')
-  if (screen === 'logins') {
-    return <Logins />
-  }
-
-  if (screen === 'login') {
-    return (
-      <Login
-        onLogin={() => setScreen('app')}
-        onSignup={() => setScreen('signup')}
-      />
-    )
-  }
-
-  if (screen === 'signup') {
-    return (
-      <Signup
-        onSignupComplete={() => setScreen('app')}
-        onBackToLogin={() => setScreen('logins')}
-      />
-    )
-  }
-
-  if (screen === 'app') {
-    return <App />
-  }
-
-  return null
-}
-
-createRoot(document.getElementById('root')).render(<Root />)
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />         
+      <Route path="/logins" element={<Logins />} />
+      
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  </BrowserRouter>
+)
